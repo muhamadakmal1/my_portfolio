7a8c2d33325af923e55064884dff74034fd58f0d
Muhamad Akmal <149946688+muhamadakmal1@users.noreply.github.com>
2025-06-09 11:28:16 +0530

    Update Certificates.jsx

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -26,6 +26,15 @@ function Certificates() {
       image: "/certificates/genai.jpg",
       link: "https://www.udemy.com/certificate/UC-3e1fa40a-27e0-4315-a623-5ac2d8f366df/",
     },
+     {
+  title: "Complete Data Science, Machine Learning, DL, NLP Bootcamp",
+  date: "June 2025",
+  description:
+    "Mastered end-to-end Data Science through a 99-hour intensive program led by industry expert Krish Naik. Gained hands-on experience in supervised & unsupervised ML, deep learning with neural networks, and advanced NLP techniques. Applied real-world datasets and deployed scalable AI solutions with modern tools and frameworks.",
+  image: "/certificates/ds.jpeg",
+  link: "https://www.udemy.com/certificate/UC-9d748652-a094-4138-9fce-f505e6c8bc26/",
+},
+    
   ];
 
   return (
